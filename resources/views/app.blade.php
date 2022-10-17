d9888c7c5c6e07e23bb59fb045696dc7a312d209
Mārtiņš Ciekurs <ciekurs.martins@gmail.com>
2022-10-17 23:39:39 +0300

    ..

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,42 +1,38 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        @auth
-            @routes
-        @elseguest
-            <script>
-                function route() { /* just template... do not delete this so app.js does not break... */ }
-            </script>
-        @endauth
+    <!-- Scripts -->
+    @auth
+        @routes
+    @elseguest
+        <script>
+            function route() { /* just template... do not delete this so app.js does not break... */ }
+        </script>
+    @endauth
 
-        @production
-            @php
-                $manifest = json_decode(File::get(public_path('build/manifest.json')), true);
-            @endphp
-            <script type="module" src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}"></script>
-            <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/js/app.js']['css'][0]) }}">
-            @env('local', 'staging')
-                @verbatim
-                    <script type="module" src="http://localhost:3000/@vite/client"></script>
-                @endverbatim
-                <script type="module" src="http://localhost:3000/resources/js/app.js"></script>
-            @endenv
-        @endproduction
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-
-{{--        @env ('local')--}}
-{{--            <script src="http://localhost:3000/browser-sync/browser-sync-client.js"></script>--}}
-{{--        @endenv--}}
-    </body>
+    @production
+        @php
+            $manifest = json_decode(File::get(public_path('build/manifest.json')), true);
+        @endphp
+        <script type="module" src="{{ asset('build/' . $manifest['resources/js/app.js']['file']) }}"></script>
+        <link rel="stylesheet" href="{{ asset('build/' . $manifest['resources/js/app.js']['css'][0]) }}">
+    @endproduction
+    @env('local')
+        @verbatim
+            <script type="module" src="http://localhost:3000/@vite/client"></script>
+        @endverbatim
+        <script type="module" src="http://localhost:3000/resources/js/app.js"></script>
+    @endenv
+</head>
+<body class="font-sans antialiased">
+@inertia
+</body>
 </html>
