11174e435d4e57b3933fac30763020cab31da9b4
Martins <ciekurs.martins@gmail.com>
2022-12-08 06:41:48 +0200

    Minor fix

--- app/Services/GoalService.php
+++ app/Services/GoalService.php
@@ -2,7 +2,10 @@
 
 namespace App\Services;
 
+use App\Models\EntityLevel;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class GoalService {
     public static function creatableByCurrentUser($user_id, $entity_id) {
@@ -98,5 +101,88 @@ class GoalService {
         }
 
         return false;
+    }
+
+    // inefficient (!!!) authorization algorithm to goals
+    // TODO improve the algorithm
+    public static function authorizeAccessingGoal(Builder $builder) {
+        $user = Auth::user();
+
+        abort_if(!$user,Response::HTTP_FORBIDDEN, '403 Forbidden');
+g
+        $isFirstLevelSupervisor = $user->supervisedEntities->pluck('entity_level_id')->contains(EntityLevel::whereNull('parent_entity_level_id')->first()->id); // aka izpilddirektors var redzēt visus mērķus
+        if ($user->hasRole('Super Admin') || $user->hasRole('Admin') || $isFirstLevelSupervisor) return $builder;
+
+        $mainEntity = $user->entity;
+        $parentEntityIds = [];
+        if ($mainEntity) {
+            $currentEntity = $mainEntity;
+            while ($currentEntity->parentEntity) {
+                $currentEntity = $currentEntity->parentEntity;
+                $parentEntityIds[] = $currentEntity->id;
+            }
+        }
+
+        $parentGoalIds = [];
+        foreach ($user->goals as $goal) {
+            $parentGoalIds[] = $goal->id;
+            $currentGoal = $goal;
+            while ($currentGoal->parentGoal) {
+                $currentGoal = $currentGoal->parentGoal;
+                $parentGoalIds[] = $currentGoal->id;
+            }
+        }
+
+        $supervisedEntities = $user->supervisedEntities;
+        $supervisedSubEntityIds = [];
+        foreach ($supervisedEntities as $supervisedEntity) {
+            $supervisedSubEntityIds[] = $supervisedEntity->id;
+            foreach ($supervisedEntity->subEntities as $subEntity) {
+                $supervisedSubEntityIds[] = $subEntity->id;
+            }
+        }
+
+        $directlySupervisedEmployees = $user->directlySupervisedEmployees;
+        $directlySupervisedEmployeeIds = [];
+        foreach ($directlySupervisedEmployees as $directlySupervisedEmployee) {
+            $directlySupervisedEmployeeIds[] = $directlySupervisedEmployee->id;
+        }
+
+        return $builder->with('entity')
+            // if any of user's goals are subgoal of another, show the parent goal of subgoal also
+            ->whereIn('id', $parentGoalIds)
+
+            // if directly supervised employee, should show its goals
+            ->orWhereIn('user_id', $directlySupervisedEmployeeIds)
+
+            // other cases with entities
+            ->orWhereHas('entity', function ($q1) use ($mainEntity, $parentEntityIds, $user, $supervisedSubEntityIds) {
+
+                // if the user is supervisor of the entity, the user is authorized to see all subgoals there
+                $q1->whereIn('id', $supervisedSubEntityIds);
+
+                // if entity's entity Level indicates to show to all
+                $q1->orWhereHas('entityLevel', function ($q2) use ($mainEntity, $parentEntityIds) {
+                    $q2->where('show_to_all', '=', 1);
+                });
+
+                // if entity's id is the user's entity_id and for the entity's level it is set that it should show to direct descendants
+                if ($mainEntity) {
+                    $q1->orWhere(function($q2) use ($mainEntity) {
+                        $q2->where('id', '=', $mainEntity->id)
+                            ->whereHas('entityLevel', function ($q3) use ($mainEntity) {
+                                $q3->where('show_to_direct_descendant', '=', 1);
+                            });
+                    });
+                }
+
+                // if it is indicated that goals of the entities should be shown to all descendants
+                $q1->orWhere(function ($q2) use ($parentEntityIds) {
+                    $q2->whereIn('id', $parentEntityIds)
+                        ->whereHas('entityLevel', function ($q3) use ($parentEntityIds) {
+                            $q3->where('show_to_descendants', '=', 1);
+                        });
+                });
+            });
     }
 }
